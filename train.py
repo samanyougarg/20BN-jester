652521ad2a30dc69c987fe0f09302aab8f3ab157
Sam <samygarg@deloitte.co.uk>
2020-04-23 16:43:43 +0100

    radhakrishna

--- train.py
+++ train.py
@@ -63,17 +63,17 @@ def main(args):
     # MODEL
 
     # # Build and compile RESNET3D model
-    net = Resnet3DBuilder.build_resnet_101(inp_shape, nb_classes, drop_rate=0.5)
-    opti = SGD(lr=0.01, momentum=0.9, decay= 0.0001, nesterov=False)
-    net.compile(optimizer=opti,
-                loss="categorical_crossentropy",
-                metrics=["accuracy"]) 
+    # net = Resnet3DBuilder.build_resnet_101(inp_shape, nb_classes, drop_rate=0.5)
+    # opti = SGD(lr=0.01, momentum=0.9, decay= 0.0001, nesterov=False)
+    # net.compile(optimizer=opti,
+    #             loss="categorical_crossentropy",
+    #             metrics=["accuracy"]) 
 
     # Build and compile CNN3D Lite model
-    # net = model.CNN3D_lite(inp_shape=inp_shape, nb_classes=nb_classes)
-    # net.compile(optimizer="adam",
-    #             loss="categorical_crossentropy",
-    #             metrics=["accuracy", "top_k_categorical_accuracy"]) 
+    net = model.CNN3D_lite(inp_shape=inp_shape, nb_classes=nb_classes)
+    net.compile(optimizer="adam",
+                loss="categorical_crossentropy",
+                metrics=["accuracy", "top_k_categorical_accuracy"]) 
 
     # if model weights file is present
     # load the model weights
