29be9252c7b8401677b8702bd6aa2b822c0d394c
Sam <samygarg@deloitte.co.uk>
2020-05-19 19:14:55 +0100

    radhakrishna

--- lib/model.py
+++ lib/model.py
@@ -12,11 +12,6 @@ from keras.layers import Input, Conv3D, MaxPooling3D, AveragePooling3D, GlobalAv
 from keras.layers.wrappers import TimeDistributed
 from keras.layers.convolutional import (Conv2D, MaxPooling2D)
 from keras import backend as K
-from keras.applications.mobilenet_v2 import MobileNetV2
-from keras.applications.mobilenet import MobileNet
-from keras.layers import Input, Flatten, Dropout, Concatenate, Activation, Dense
-from keras.layers import Convolution3D, MaxPooling3D, AveragePooling3D
-from keras.layers import GlobalMaxPooling3D, GlobalAveragePooling3D
 
 
 def CNN3D(inp_shape, nb_classes, k_size=(3,3,3), drop_rate=0):
@@ -90,7 +85,7 @@ def RadhaKrishna(inp_shape, nb_classes):
     # From https://github.com/patrickjohncyh/ibm-waldo/blob/master/2-MachineLearning/server-training/Models.py
     """
 
-    model = tf.keras.Sequential()
+    model = Sequential()
 
     model.add(InputLayer(input_shape=inp_shape))
     model.add(Conv3D(32, 3,strides=(1,2,2), activation='relu', padding='same', name='conv1', input_shape=inp_shape))
